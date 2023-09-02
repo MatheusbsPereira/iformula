b91a824e9ca011be88b3aa382feb3a331be42b99
MatheusbsPereira <89664879+MatheusbsPereira@users.noreply.github.com>
2023-09-02 16:50:38 -0300

    editando validacao de nome nutriente em edicao

--- app/Livewire/CrudNutrienteComponent.php
+++ app/Livewire/CrudNutrienteComponent.php
@@ -12,6 +12,7 @@ class CrudNutrienteComponent extends Component
     public string $nome;
     public string $tag;
     public string $unidade;
+    public string $first_nome;
     public function render()
     {
         return view('livewire.crud-nutriente-component');
@@ -21,13 +22,18 @@ class CrudNutrienteComponent extends Component
         $nutriente = Nutriente::find($id);
         $this->id = $nutriente->id;
         $this->nome = $nutriente->nome;
+        $this->first_nome = $this->nome ;
         $this->tag = $nutriente->tag;
         $this->unidade = $nutriente->unidade;
         $this->dispatch('showModal');
     }
     public function save( )
     {
-        $this->validate($this->rules());
+        if ($this->nome != $this->first_nome) {
+           $this->validate($this->rulesNewName());
+        }else{
+            $this->validate($this->rules());
+        }
         $nutriente = Nutriente::find($this->id);
         $nutriente->nome =$this->nome;
         $nutriente->tag =$this->tag;
@@ -35,10 +41,18 @@ class CrudNutrienteComponent extends Component
         $nutriente->save();
         return redirect()->to(route('nutriente.index'));
     }
-    public function rules()
+    public function rulesNewName()
     {
         return [
             'nome' => ['required', 'max:50', new NomeNutriente],
+            'unidade' => ['required', 'max:6'],
+            'tag' => ['required', 'max:10'],
+        ];
+    }
+    public function rules()
+    {
+        return [
+            'nome' => ['required', 'max:50',],
             'unidade' => ['required', 'max:6'],
             'tag' => ['required', 'max:10'],
         ];
