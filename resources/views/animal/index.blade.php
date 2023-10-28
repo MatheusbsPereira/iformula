108d78ff41869ff621385947299fe9b8ba7765e4
Mexuz <controleryan@gmail.com>
2023-10-28 11:49:18 -0300

    Exclusões de itens inutilizados

--- resources/views/animal/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('titulo','Animais')
-@section('content')
-    <livewire:animal_component />
-@endsection
-@section('scriptcss')
-    <link rel="stylesheet" href="{{ asset('css/style-animais-view.css') }}"/>
-@endsection
--- resources/views/animal/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('titulo', "Ingrediente $animal->nome")
-@section('content')
-    <div class="container">
-        <livewire:show_animal_component :animal="$animal" />
-    </div>
-@endsection
